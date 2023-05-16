b31bd3bfbfcedd16aa75ede7176ed365954069b5
Juanmaiglesias10 <juanmaiglesiascasla@gmail.com>
2023-05-16 19:21:43 -0300

    Agrego ejercicio de Celsius a Far

--- EjerciciosLibroDeC/CelsiusAFar.c
+++ EjerciciosLibroDeC/CelsiusAFar.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /* imprime la tabla Fahrenheit-Celsius
 para fahr = 0, 20, ..., 300; versión de punto flotante */
-int main()
-{
-float fahr, celsius;
+int main() {
+int fahr, celsius;
 int lower, upper, step;
 
 lower = 0; /* límite superior de la tabla de temperaturas */
@@ -12,9 +11,10 @@ step = 20; /* tamaño del incremento */
 fahr = lower;
 
     while (fahr <= upper) {
-        celsius = (5.0/9.0) * (fahr-32.0);
-        printf("%3.0f %6.1f\n, fahr, celsius");
+        celsius = 5 * (fahr-32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
         fahr = fahr + step;
         }
+
 return 0; 
 }
